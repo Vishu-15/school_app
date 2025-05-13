ab50fa8c6b9920d97094bbad0bf3134008a440dd
Vishu-15 <vsbangalore15@gmail.com>
2025-05-13 22:31:11 +0530

    change db credentials

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const db = sql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: process.env.MYSQL_DB,
     port: process.env.MYSQL_PORT,
     ssl: {
         rejectUnauthorized: false, 
